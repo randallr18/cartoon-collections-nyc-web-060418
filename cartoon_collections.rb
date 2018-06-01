5b2f4b679b71a23cc2c80dcf243cfa8e0b7b1cc8
Risher Randall <randallr18@mail.wlu.edu>
2018-06-01 00:19:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,8 @@ def summon_captain_planet(calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls
 end
 
 def find_the_cheese# code an argument here
