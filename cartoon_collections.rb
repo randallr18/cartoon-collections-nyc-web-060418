67e2fd30cfb8ba968c088b6b8ec35745af9961c0
Risher Randall <randallr18@mail.wlu.edu>
2018-06-01 00:31:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,6 +25,6 @@ end
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
   snacks.find do |grub|
-    cheese_types.index(grub) > -1
+    if cheese_types.index(grub) > -1
   end
 end
