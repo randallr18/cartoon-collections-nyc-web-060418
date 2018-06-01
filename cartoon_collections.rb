9796aedfcd505cab364b887b0cc421d20d43c570
Risher Randall <randallr18@mail.wlu.edu>
2018-06-01 00:36:06 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  number = 1
+  names.each do |name|
+    puts "#{number}. #{name}"
+    number += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.collect do |x|
+    first = x[0].upcase
+    array = x.split(//)
+    array.shift
+    array.unshift(first)
+    array.join('') + '!'
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |word|
+    word.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find do |grub|
+    cheese_types.include?(grub)
+  end
 end
