fa1c9b692a5f188117d0d96223c690abd4a02b48
Risher Randall <randallr18@mail.wlu.edu>
2018-06-01 00:23:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,9 @@ def summon_captain_planet(calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.any do |word|
-    
+  calls.any? do |word|
+    word.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
